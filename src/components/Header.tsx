d176b90b94afc14dc5e9e6366f46f34aad5719e5
Nikoloz <nikoloz.palagashvili@gmail.com>
2023-11-03 11:27:55 +0400

    hedaer responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import { MyAutoIcon } from "./icons/Myauto";
 
 export const Header = () => (
-	<header className="flex items-center justify-center h-[80px] w-screen bg-white">
-		<div className="md:w-[100rem]">
+	<header className="flex items-center justify-start pl-[1.6rem] md:justify-center h-[80px] w-screen bg-white">
+		<div className="md:w-[104rem]">
 			<MyAutoIcon />
 		</div>
 	</header>
